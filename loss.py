07ba73fd4dcf7bcc05ba400890b39f56b1b5b09a
Seyong Kim <seyong@yonsei.ac.kr>
2022-06-08 23:40:14 +0900

    modify mean to sum

--- loss.py
+++ loss.py
@@ -2,5 +2,6 @@ import torch
 
 
 def kl_divergence(mu: torch.tensor, log_var: torch.tensor):
-    kl_div_element = mu.pow(2).add_(log_var.exp()).mul_(-1).add_(1).add_(log_var)
-    return torch.mean(kl_div_element).mul_(-0.5)
+    # output = -1/2 (log_var + 1 -torch.add(torch.pow(mu, 2), torch.exp(log_var)))
+    KLD_element = mu.pow(2).add_(log_var.exp()).mul_(-1).add_(1).add_(log_var)
+    return torch.sum(KLD_element).mul_(-0.5)
